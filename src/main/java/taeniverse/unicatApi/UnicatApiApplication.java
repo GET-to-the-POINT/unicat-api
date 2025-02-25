2bb26088142975b26c4ab470c810eeb8aa29420c
rimkogi@naver.com <rimkogi@naver.com>
2025-02-25 15:15:14 +0900

    동영상 스케줄링 1차

--- src/main/java/taeniverse/unicatApi/UnicatApiApplication.java
+++ src/main/java/taeniverse/unicatApi/UnicatApiApplication.java
@@ -2,7 +2,10 @@ package taeniverse.unicatApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+//유림 추가
+@EnableScheduling
 @SpringBootApplication
 public class UnicatApiApplication {
 
