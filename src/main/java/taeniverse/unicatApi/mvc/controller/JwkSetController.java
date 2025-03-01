e971b8fa2ef248af110b5d5d8f48466447b9437c
XIYO <bunny@xiyo.dev>
2025-03-01 23:58:08 +0900

    :sparkles: 사인 컨트롤러에 폼 기능 추가

--- src/main/java/taeniverse/unicatApi/mvc/controller/JwkSetController.java
+++ src/main/java/taeniverse/unicatApi/mvc/controller/JwkSetController.java
@@ -6,6 +6,8 @@ import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.KeyUse;
 import com.nimbusds.jose.jwk.RSAKey;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -32,7 +34,33 @@ public class JwkSetController {
             summary = "JWK Set 조회",
             description = "현재 RSA 공개키를 기반으로 생성된 JWK Set 정보를 반환합니다.",
             responses = {
-                    @ApiResponse(responseCode = "200", description = "JWK Set 조회 성공")
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "JWK Set 조회 성공",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "JWK Set",
+                                                    summary = "예시 JWK Set",
+                                                    value = """
+{
+  "keys": [
+    {
+      "kty": "RSA",
+      "e": "AQAB",
+      "use": "sig",
+      "kid": "rsa-key-id",
+      "alg": "RS256",
+      "n": "1n4iFl-u0JR0Fdm62KFkaRZv-i6o3fBGyPpwpuoNwP84hEDgwfuDlgN9S6Hqaz_GNDxZqRlAdzOFz4DRJQOo_fPh..."
+    }
+  ]
+}
+"""
+                                            )
+                                    }
+                            )
+                    )
             }
     )
     public Map<String, Object> getJwks() {
