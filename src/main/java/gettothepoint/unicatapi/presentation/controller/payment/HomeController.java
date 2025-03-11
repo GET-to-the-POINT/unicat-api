dc900108d66ad19c9f6b84cd5461ec4c04dec342
hyeri <so728@naver.com>
2025-03-11 16:22:45 +0900

    원래 결제만 성공 버전

--- src/main/java/gettothepoint/unicatapi/presentation/controller/payment/HomeController.java
+++ src/main/java/gettothepoint/unicatapi/presentation/controller/payment/HomeController.java
@@ -1,10 +1,6 @@
 package gettothepoint.unicatapi.presentation.controller.payment;
 
-import gettothepoint.unicatapi.application.service.MemberService;
-import gettothepoint.unicatapi.domain.entity.Member;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +13,6 @@ import java.util.UUID;
 public class HomeController {
 
     private final AppProperties appProperties;
-    private final MemberService memberService;
 
     @GetMapping("/todos")
     public String home() {
@@ -25,14 +20,11 @@ public class HomeController {
     }
 
     @GetMapping("/api/payment")
-    public String showPaymentPage(Model model, @AuthenticationPrincipal Jwt jwt) {
+    public String showPaymentPage(Model model) {
         model.addAttribute("clientKey", appProperties.toss().clientKey());
-
-        String email = jwt.getClaim("email");
-        Member member = memberService.findByEmail(email);
-        String customerKey = member.getCustomerKey();
-
+        String customerKey = UUID.randomUUID().toString();
         model.addAttribute("customerKey", customerKey);
+
         return "payment";
     }
 }
