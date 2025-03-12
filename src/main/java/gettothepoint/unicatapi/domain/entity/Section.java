bbbf36331bd75daeaaeedb7941f143c11b172637
wooyeon <kimsy960119@gmail.com>
2025-03-12 15:47:03 +0900

    :wrench: section 시스템 컬럼 추가

--- src/main/java/gettothepoint/unicatapi/domain/entity/Section.java
+++ src/main/java/gettothepoint/unicatapi/domain/entity/Section.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
 @Getter
 @NoArgsConstructor
 @Entity
@@ -21,6 +24,20 @@ public class Section {
     @ManyToOne
     @JoinColumn
     private Project project;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 
     @Builder
     public Section(String uploadImageUrl, String script, Project project) {
