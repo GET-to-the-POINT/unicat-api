801bd0c9c639d5723767a6a1cdbc0dc56d9e58a7
XIYO <bunny@xiyo.dev>
2025-03-13 10:42:57 +0900

    :art: Oauth2 리디렉트 구현 및 필요 없는 기본 구현체 제거

---
+++ src/main/java/gettothepoint/unicatapi/infrastructure/security/oAuth2Client/authorizedClient/HttpCookieOAuth2AuthorizationRequestRepository.java
@@ -0,0 +1,79 @@
+package gettothepoint.unicatapi.infrastructure.security.oAuth2Client.authorizedClient;
+
+import gettothepoint.unicatapi.common.util.CookieUtil;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+import org.springframework.web.util.WebUtils;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Base64;
+
+@Component
+public class HttpCookieOAuth2AuthorizationRequestRepository implements AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
+
+    public static final String OAUTH2_AUTH_REQUEST = HttpCookieOAuth2AuthorizationRequestRepository.class.getName() + "oauth2_auth_request";
+    public static final String REDIRECT_URI = HttpCookieOAuth2AuthorizationRequestRepository.class.getName() + "redirect";
+    private static final int COOKIE_EXPIRE_SECONDS = 180;
+
+    @Override
+    public OAuth2AuthorizationRequest loadAuthorizationRequest(HttpServletRequest request) {
+        Assert.notNull(request, "request cannot be null");
+        Cookie cookie = WebUtils.getCookie(request, OAUTH2_AUTH_REQUEST);
+        if (cookie == null) {
+            return null;
+        }
+        return deserialize(cookie.getValue());
+    }
+
+    @Override
+    public void saveAuthorizationRequest(OAuth2AuthorizationRequest authorizationRequest, HttpServletRequest request, HttpServletResponse response) {
+        if (authorizationRequest == null) {
+            CookieUtil.deleteCookie(request, response, OAUTH2_AUTH_REQUEST);
+            CookieUtil.deleteCookie(request, response, REDIRECT_URI);
+            return;
+        }
+        String serializedAuthRequest = serialize(authorizationRequest);
+        CookieUtil.addCookie(response, OAUTH2_AUTH_REQUEST, serializedAuthRequest, COOKIE_EXPIRE_SECONDS);
+
+        String redirectUriAfterSignIn = request.getParameter(REDIRECT_URI);
+        if (redirectUriAfterSignIn != null && !redirectUriAfterSignIn.isEmpty()) {
+            CookieUtil.addCookie(response, REDIRECT_URI, redirectUriAfterSignIn, COOKIE_EXPIRE_SECONDS);
+        }
+    }
+
+    @Override
+    public OAuth2AuthorizationRequest removeAuthorizationRequest(HttpServletRequest request, HttpServletResponse response) {
+        OAuth2AuthorizationRequest authorizationRequest = loadAuthorizationRequest(request);
+        CookieUtil.deleteCookie(request, response, OAUTH2_AUTH_REQUEST);
+        return authorizationRequest;
+    }
+
+    private String serialize(OAuth2AuthorizationRequest object) {
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+             ObjectOutputStream oos = new ObjectOutputStream(baos)) {
+            oos.writeObject(object);
+            return Base64.getUrlEncoder().encodeToString(baos.toByteArray());
+        } catch (IOException e) {
+            throw new IllegalStateException("Failed to serialize OAuth2AuthorizationRequest", e);
+        }
+    }
+
+    private OAuth2AuthorizationRequest deserialize(String cookieValue) {
+        try (ByteArrayInputStream bais = new ByteArrayInputStream(Base64.getUrlDecoder().decode(cookieValue));
+             ObjectInputStream ois = new ObjectInputStream(bais)) {
+            return (OAuth2AuthorizationRequest) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new IllegalStateException("Failed to deserialize OAuth2AuthorizationRequest", e);
+        }
+    }
+
+}
