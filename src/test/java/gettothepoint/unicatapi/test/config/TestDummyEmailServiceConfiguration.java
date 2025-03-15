ca373588a357f71a526850ca27096161e5d9767a
hyeri <so728@naver.com>
2025-03-15 22:23:58 +0900

    :test_tube: 테스트 케이스

---
+++ src/test/java/gettothepoint/unicatapi/test/config/TestDummyEmailServiceConfiguration.java
@@ -0,0 +1,32 @@
+package gettothepoint.unicatapi.test.config;
+
+import gettothepoint.unicatapi.application.service.email.EmailService;
+import gettothepoint.unicatapi.domain.entity.member.Member;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+@TestConfiguration
+public class TestDummyEmailServiceConfiguration {
+
+    @Bean
+    @Primary
+    public EmailService emailService() {
+        return new EmailService(null, null, null) {
+            @Override
+            public void sendEmail(String recipient, String subject, String content) {
+                // 테스트에서는 실제 이메일 전송을 수행하지 않음
+            }
+
+            @Override
+            public void sendVerificationEmail(Member member) {
+                // 테스트에서는 실제 이메일 전송을 수행하지 않음
+            }
+
+            @Override
+            public void sendPasswordResetEmail(String email, Long memberId) {
+                // 테스트에서는 실제 이메일 전송을 수행하지 않음
+            }
+        };
+    }
+}
