2717988b78578816a195f78fcc45e6413447fa62
hyeri <so728@naver.com>
2025-03-15 16:42:49 +0900

    :sparkles: 현재 패스워드 확인 기능

--- src/main/java/gettothepoint/unicatapi/application/service/password/PasswordService.java
+++ src/main/java/gettothepoint/unicatapi/application/service/password/PasswordService.java
@@ -4,6 +4,7 @@ import gettothepoint.unicatapi.application.service.MemberService;
 import gettothepoint.unicatapi.application.service.email.EmailService;
 import gettothepoint.unicatapi.domain.entity.member.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,7 +14,7 @@ public class PasswordService {
 
     private final MemberService memberService;
     private final EmailService emailService;
-
+    private final PasswordEncoder passwordEncoder;
 
     public void sendPasswordResetEmail(String email) {
         Member member = memberService.findByEmail(email);
@@ -26,5 +27,10 @@ public class PasswordService {
             throw new IllegalArgumentException("새 비밀번호와 확인이 일치하지 않습니다.");
         }
         memberService.updatePassword(email, newPassword);
+    }
+
+    public boolean verifyCurrentPassword(String email, String currentPassword) {
+        Member member = memberService.findByEmail(email);
+        return passwordEncoder.matches(currentPassword, member.getPassword());
     }
 }
---
+++ src/main/java/gettothepoint/unicatapi/presentation/password/PasswordController.java
@@ -0,0 +1,29 @@
+package gettothepoint.unicatapi.presentation.password;
+
+import gettothepoint.unicatapi.application.service.password.PasswordService;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class PasswordController {
+
+    private final PasswordService passwordService;
+
+    @PostMapping("/me/password/verify")
+    public void verifyCurrentPassword(
+            @AuthenticationPrincipal Jwt jwt,
+            @NotEmpty @RequestParam String currentPassword) {
+        String email = jwt.getClaimAsString("name");
+        passwordService.verifyCurrentPassword(email, currentPassword);
+    }
+
+}
+
