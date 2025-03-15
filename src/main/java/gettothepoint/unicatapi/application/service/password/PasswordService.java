62527bfc5ebfe775f458a6e0de71ff7e7a37ec58
Soheyri <so728@naver.com>
2025-03-15 17:58:14 +0900

    PR-Password-post-verify (#88)

--- src/main/java/gettothepoint/unicatapi/application/service/password/PasswordService.java
+++ src/main/java/gettothepoint/unicatapi/application/service/password/PasswordService.java
@@ -4,8 +4,11 @@ import gettothepoint.unicatapi.application.service.MemberService;
 import gettothepoint.unicatapi.application.service.email.EmailService;
 import gettothepoint.unicatapi.domain.entity.member.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +16,7 @@ public class PasswordService {
 
     private final MemberService memberService;
     private final EmailService emailService;
-
+    private final PasswordEncoder passwordEncoder;
 
     public void sendPasswordResetEmail(String email) {
         Member member = memberService.findByEmail(email);
@@ -26,5 +29,14 @@ public class PasswordService {
             throw new IllegalArgumentException("새 비밀번호와 확인이 일치하지 않습니다.");
         }
         memberService.updatePassword(email, newPassword);
+    }
+
+    public void verifyCurrentPassword(String email, String currentPassword) {
+        Member member = memberService.findByEmail(email);
+        boolean isSame = passwordEncoder.matches(currentPassword, member.getPassword());
+
+        if (!isSame) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다.");
+        }
     }
 }
