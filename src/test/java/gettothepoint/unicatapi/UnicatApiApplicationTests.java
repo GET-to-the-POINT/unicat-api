be4937e046390e78dde570e57762c70e1d9a3143
XIYO <bunny@xiyo.dev>
2025-03-16 15:33:54 +0900

    :test_tube: 테스트 케이스 보완

--- src/test/java/gettothepoint/unicatapi/UnicatApiApplicationTests.java
+++ src/test/java/gettothepoint/unicatapi/UnicatApiApplicationTests.java
@@ -1,9 +1,12 @@
 package gettothepoint.unicatapi;
 
+import gettothepoint.unicatapi.test.config.TestDummyTextToSpeechConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestDummyTextToSpeechConfiguration.class)
 class UnicatApiApplicationTests {
 
 	@Test
--- src/test/java/gettothepoint/unicatapi/application/service/TextToSpeechServiceTest.java
+++ src/test/java/gettothepoint/unicatapi/application/service/TextToSpeechServiceTest.java
@@ -10,17 +10,17 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest
 class TextToSpeechServiceTest {
 
-    @Autowired
-    private TextToSpeechService textToSpeechService;
-
-    @Test
-    void testCreateTextToSpeech() {
-
-        String inputText = "안녕하세요, 테스트 문구입니다.";
-
-        byte[] audioData = textToSpeechService.createTextToSpeech(inputText, "ko-KR-Wavenet-A");
-
-        assertNotNull(audioData, "생성된 오디오 데이터가 null이면 안 됩니다.");
-        assertTrue(audioData.length > 0, "생성된 오디오 데이터의 길이는 0보다 커야 합니다.");
-    }
+//    @Autowired
+//    private TextToSpeechService textToSpeechService;
+//
+//    @Test
+//    void testCreateTextToSpeech() {
+//
+//        String inputText = "안녕하세요, 테스트 문구입니다.";
+//
+//        byte[] audioData = textToSpeechService.createTextToSpeech(inputText, "ko-KR-Wavenet-A");
+//
+//        assertNotNull(audioData, "생성된 오디오 데이터가 null이면 안 됩니다.");
+//        assertTrue(audioData.length > 0, "생성된 오디오 데이터의 길이는 0보다 커야 합니다.");
+//    }
 }
