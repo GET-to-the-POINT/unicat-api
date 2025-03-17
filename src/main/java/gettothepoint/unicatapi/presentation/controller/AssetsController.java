6441dc857e0872b09ce932085d10e65db44976fa
XIYO <bunny@xiyo.dev>
2025-03-17 23:07:39 +0900

    :truck: 스토리지 컨트롤러를 에셋을 참조하는 컨트롤러로 변경

---
+++ src/main/java/gettothepoint/unicatapi/presentation/controller/AssetsController.java
@@ -0,0 +1,23 @@
+package gettothepoint.unicatapi.presentation.controller;
+
+import gettothepoint.unicatapi.application.service.storage.SampleVoiceService;
+import gettothepoint.unicatapi.domain.dto.storage.SampleVoice;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Assets", description = "Assets API")
+@RestController
+@RequestMapping("/assets")
+@RequiredArgsConstructor
+public class AssetsController {
+
+    private final SampleVoiceService sampleVoiceService;
+
+    @GetMapping("/voices")
+    public SampleVoice[] getVoices() {
+        return sampleVoiceService.getSampleVoices();
+    }
+}
