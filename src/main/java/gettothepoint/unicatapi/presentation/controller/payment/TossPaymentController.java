243d11a2a6f52c2e8fe2da058aebedc349989729
Soheyri <so728@naver.com>
2025-03-24 14:37:30 +0900

    PR 141-feat-결제-리디렉션 (#145)
    Co-authored-by: 김태희 <bunny@xiyo.dev>

--- src/main/java/gettothepoint/unicatapi/presentation/controller/payment/TossPaymentController.java
+++ src/main/java/gettothepoint/unicatapi/presentation/controller/payment/TossPaymentController.java
@@ -1,14 +1,18 @@
 package gettothepoint.unicatapi.presentation.controller.payment;
 
 import gettothepoint.unicatapi.application.service.payment.BillingService;
+import gettothepoint.unicatapi.application.service.payment.OrderService;
 import gettothepoint.unicatapi.application.service.payment.PaymentService;
+import gettothepoint.unicatapi.common.propertie.AppProperties;
+import gettothepoint.unicatapi.domain.constant.payment.SubscriptionPlan;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import java.io.IOException;
 
 @Tag(name = "Subscription - Toss", description = "Toss Payments API")
 @RestController
@@ -18,13 +22,19 @@ public class TossPaymentController {
 
     private final BillingService billingService;
     private final PaymentService paymentService;
+    private final OrderService orderService;
+    private final AppProperties appProperties;
 
     @GetMapping("/approve")
-    public Map<String, Object> approveAutoPayment(
-            @AuthenticationPrincipal Jwt jwt, @RequestParam String authKey) {
+    public void approveAutoPayment(
+            @AuthenticationPrincipal Jwt jwt,
+            @RequestParam String authKey,
+            HttpServletResponse response) throws IOException {
         String email = jwt.getClaim("email");
+        orderService.create(email, SubscriptionPlan.PREMIUM);//todo 가주문 나중에 subscription컨트롤러 옮길수도 있음
         billingService.saveBillingKey(authKey, email);
-        return paymentService.approveAutoPayment(email);
+        paymentService.approveAutoPayment(email);
+        response.sendRedirect(appProperties.frontend().url());
     }
 
 }
