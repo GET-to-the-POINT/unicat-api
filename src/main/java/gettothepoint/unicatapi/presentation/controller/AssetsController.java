6ad12a61f66e3d7ed27d6152eda1ef0049438a89
wooyeon <147980284+wjddbs-yoon@users.noreply.github.com>
2025-03-24 12:11:37 +0900

    PR-bug-인공지능-이미지-생성-잘못됨 (#142)
    AI이미지 모델 변경(번역 삭제) 및 ImageStyle 추가

--- src/main/java/gettothepoint/unicatapi/presentation/controller/AssetsController.java
+++ src/main/java/gettothepoint/unicatapi/presentation/controller/AssetsController.java
@@ -1,8 +1,10 @@
 package gettothepoint.unicatapi.presentation.controller;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import gettothepoint.unicatapi.application.service.storage.SampleVoiceService;
 import gettothepoint.unicatapi.domain.dto.storage.SampleVoice;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +21,26 @@ public class AssetsController {
     @GetMapping("/voices")
     public SampleVoice[] getVoices() {
         return sampleVoiceService.getSampleVoices();
+    }
+
+    @GetMapping("/imageStyles")
+    public ImageStyle[] getImageStyles() {
+        return ImageStyle.values();
+    }
+
+    @JsonFormat(shape = JsonFormat.Shape.OBJECT)
+    @Getter
+    @RequiredArgsConstructor
+    public enum ImageStyle {
+        PHOTO("Photo", "실제 사진처럼 보이는 이미지"),
+        ILLUSTRATION("Illustration", "일러스트"),
+        PENCIL_SKETCH("Pencil sketch", "손그림"),
+        DIGITAL_ART("Digital Art", "디지털 아트"),
+        ANIME("Anime", "애니메이션"),
+        WATERCOLOR("Watercolor", "수채화"),
+        CONCEPT_ART("Concept Art", "컨셉 아트");
+
+        private final String value;
+        private final String name;
     }
 }
