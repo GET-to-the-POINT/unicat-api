d674c145268e7759e96dba19aa67be27ce056511
wooyeon <kimsy960119@gmail.com>
2025-03-24 11:21:56 +0900

    :art: imageStyle 추가

--- src/main/java/gettothepoint/unicatapi/domain/dto/project/PromptRequest.java
+++ src/main/java/gettothepoint/unicatapi/domain/dto/project/PromptRequest.java
@@ -2,7 +2,6 @@ package gettothepoint.unicatapi.domain.dto.project;
 
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
-public record PromptRequest(@NotBlank @Size(min = 20, message = "20자 이상을 입력하세요.") String prompt) {
+public record PromptRequest(@NotBlank String prompt) {
 }
