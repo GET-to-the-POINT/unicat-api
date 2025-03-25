26df90dd9442a90a1ceae7c3f531c601c634e7f0
XIYO <bunny@xiyo.dev>
2025-03-25 22:17:13 +0900

    :recycle:

--- src/main/java/gettothepoint/unicatapi/application/service/AuthService.java
+++ src/main/java/gettothepoint/unicatapi/application/service/AuthService.java
@@ -3,10 +3,9 @@ package gettothepoint.unicatapi.application.service;
 import gettothepoint.unicatapi.application.service.email.EmailService;
 import gettothepoint.unicatapi.application.service.member.MemberService;
 import gettothepoint.unicatapi.common.util.JwtUtil;
-import gettothepoint.unicatapi.domain.dto.sign.SignInDto;
+import gettothepoint.unicatapi.domain.dto.sign.SignInRequest;
 import gettothepoint.unicatapi.domain.dto.sign.SignUpRequest;
 import gettothepoint.unicatapi.domain.entity.member.Member;
-import gettothepoint.unicatapi.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -19,7 +18,6 @@ import org.springframework.web.server.ResponseStatusException;
 @RequiredArgsConstructor
 public class AuthService {
 
-    private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
     private final MessageSource messageSource;
@@ -27,35 +25,27 @@ public class AuthService {
     private final EmailService emailService;
 
     public String signUp(SignUpRequest signUpRequest) {
-        validateEmail(signUpRequest.email());
         Member member = memberService.create(signUpRequest.email(), signUpRequest.password(), signUpRequest.name(), signUpRequest.phoneNumber());
         emailService.sendVerificationEmail(member);
         return generateAndAddJwtToken(member);
     }
 
-    public String signIn(SignInDto signInDto) {
-        Member member = validateCredentials(signInDto.email(), signInDto.password());
+    public String signIn(SignInRequest signInRequest) {
+        Member member = memberService.getOrElseThrow(signInRequest.email());
+        validateCredentials(member, signInRequest.password());
         return generateAndAddJwtToken(member);
     }
 
-    private void validateEmail(String email) {
-        if (Boolean.TRUE.equals(memberRepository.existsByEmail(email))) {
-            String errorMessage = messageSource.getMessage("error.email.in.use", null, "", LocaleContextHolder.getLocale());
-            throw new ResponseStatusException(HttpStatus.CONFLICT, errorMessage);
-        }
-    }
 
-    private Member validateCredentials(String email, String password) {
-        Member member = memberRepository.findByEmail(email).orElse(null);
-        if (member == null || !passwordEncoder.matches(password, member.getPassword())) {
+    private void validateCredentials(Member member, String password) {
+        if (!passwordEncoder.matches(password, member.getPassword())) {
             String errorMessage = messageSource.getMessage("error.invalid.credentials", null, "", LocaleContextHolder.getLocale());
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, errorMessage);
         }
-        return member;
     }
 
     private String generateAndAddJwtToken(Member member) {
-        return jwtUtil.generateJwtToken(member.getId(), member.getEmail(), member.getSubscription().getSubscriptionPlan());
+        return jwtUtil.generateJwtToken(member.getId(), member.getEmail(), member.getSubscription().getSubscriptionPlan().name());
     }
 
 }
