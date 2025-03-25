66de9a8fa80636e012e3de696bbf0ae8dfdba8fb
hyeri <so728@naver.com>
2025-03-25 21:24:44 +0900

    :recycle: 이메일 스웨거 설명 추가

--- src/main/java/gettothepoint/unicatapi/presentation/controller/member/EmailController.java
+++ src/main/java/gettothepoint/unicatapi/presentation/controller/member/EmailController.java
@@ -2,10 +2,12 @@ package gettothepoint.unicatapi.presentation.controller.member;
 
 import gettothepoint.unicatapi.application.service.member.MemberService;
 import gettothepoint.unicatapi.common.util.JwtUtil;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = " - Email", description = "이메일 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/members")
