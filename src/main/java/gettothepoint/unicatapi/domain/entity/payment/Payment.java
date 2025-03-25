f6a09f77347119dfd6b4b0549b95befff825902d
XIYO <bunny@xiyo.dev>
2025-03-25 22:21:03 +0900

    :recycle:

--- src/main/java/gettothepoint/unicatapi/domain/entity/payment/Payment.java
+++ src/main/java/gettothepoint/unicatapi/domain/entity/payment/Payment.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 @Entity
 public class Payment extends BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     @Column(nullable = false, updatable = false)
