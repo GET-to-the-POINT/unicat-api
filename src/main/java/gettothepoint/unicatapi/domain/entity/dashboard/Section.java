16c4686594befac8a8fd8a28212b39a0f9175c25
XIYO <bunny@xiyo.dev>
2025-03-25 22:21:03 +0900

    :recycle:

--- src/main/java/gettothepoint/unicatapi/domain/entity/dashboard/Section.java
+++ src/main/java/gettothepoint/unicatapi/domain/entity/dashboard/Section.java
@@ -13,7 +13,7 @@ import lombok.Setter;
 public class Section extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Setter
     private Long id;
 
--- src/main/java/gettothepoint/unicatapi/domain/entity/payment/Subscription.java
+++ src/main/java/gettothepoint/unicatapi/domain/entity/payment/Subscription.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 public class Subscription extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     private LocalDateTime startDate;
