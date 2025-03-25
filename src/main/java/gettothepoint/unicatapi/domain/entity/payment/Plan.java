960bdc33b31372c1930a1a877e033e822ae668e0
XIYO <bunny@xiyo.dev>
2025-03-25 22:28:52 +0900

    :recycle:

--- src/main/java/gettothepoint/unicatapi/domain/entity/payment/Plan.java
+++ src/main/java/gettothepoint/unicatapi/domain/entity/payment/Plan.java
@@ -22,4 +22,10 @@ public class Plan extends BaseEntity {
     private String description;
 
     private String price;
+
+    private Long aiImageCount;
+
+    private Long aiScriptCount;
+
+    private Long artifactCount;
 }
