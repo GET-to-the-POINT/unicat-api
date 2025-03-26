c438028f02da7af84ea878c57c3c5cacc8f0d1bb
rimkogi@naver.com <rimkogi@naver.com>
2025-03-26 18:10:30 +0900

    :feat: atifact 비동기

--- src/main/java/gettothepoint/unicatapi/presentation/controller/project/ProgressController.java
+++ src/main/java/gettothepoint/unicatapi/presentation/controller/project/ProgressController.java
@@ -1,6 +1,8 @@
 package gettothepoint.unicatapi.presentation.controller.project;
 
 import gettothepoint.unicatapi.infrastructure.progress.ProgressManager;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +13,15 @@ import java.util.concurrent.Executors;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Project - Progress", description = "업로드 진행률 관련 API")
 public class ProgressController {
 
     private final ProgressManager progressManager;
 
+    @Operation(
+            summary = "샘플 진행률 SSE 테스트",
+            description = "샘플입니다."
+    )
     @GetMapping("/progress")
     public SseEmitter progress() {
         SseEmitter emitter = new SseEmitter(3 * 60 * 1000L);
@@ -34,6 +41,10 @@ public class ProgressController {
         return emitter;
     }
 
+    @Operation(
+            summary = "프로젝트 업로드 진행률 구독",
+            description = "특정 프로젝트의 유튜브 업로드 진행률을 SSE 방식으로 구독합니다."
+    )
     @GetMapping("/{projectId}/progress")
     public SseEmitter subscribe(@PathVariable Long projectId) {
         System.out.println(" [SSE 연결 생성] projectId = " + projectId);
