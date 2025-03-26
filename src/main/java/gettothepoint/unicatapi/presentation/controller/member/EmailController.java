20039e6761e4108daa695cb701c96f484a23d22d
XIYO <bunny@xiyo.dev>
2025-03-26 10:39:47 +0900

    :sparkles: 이메일 인증 후 리디렉트 추가

--- src/main/java/gettothepoint/unicatapi/presentation/controller/member/EmailController.java
+++ src/main/java/gettothepoint/unicatapi/presentation/controller/member/EmailController.java
@@ -1,10 +1,16 @@
 package gettothepoint.unicatapi.presentation.controller.member;
 
 import gettothepoint.unicatapi.application.service.member.MemberService;
+import gettothepoint.unicatapi.common.propertie.AppProperties;
 import gettothepoint.unicatapi.common.util.JwtUtil;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.net.URL;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,11 +19,15 @@ public class EmailController {
 
     private final MemberService memberService;
     private final JwtUtil jwtUtil;
+    private final AppProperties appProperties;
 
     @GetMapping("/email")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void verifyEmail(@RequestParam("token") String token) {
+    public void verifyEmail(@RequestParam("token") String token, HttpServletResponse response) throws IOException {
         String email = jwtUtil.getEmailFromToken(token);
         memberService.verifyEmail(email);
+
+        String frontend = appProperties.frontend().url();
+        response.sendRedirect(frontend);
     }
 }
