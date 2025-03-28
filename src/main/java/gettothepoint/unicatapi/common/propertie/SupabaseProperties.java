c9e9fa6b7ed66603fca1944622be6453873b0126
hyeri <so728@naver.com>
2025-03-28 22:56:56 +0900

    :recycle: supabase 스펠링

--- src/main/java/gettothepoint/unicatapi/common/propertie/SupabaseProperties.java
+++ src/main/java/gettothepoint/unicatapi/common/propertie/SupabaseProperties.java
@@ -2,7 +2,7 @@ package gettothepoint.unicatapi.common.propertie;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "app.superb")
+@ConfigurationProperties(prefix = "app.supabase")
 public record SupabaseProperties(String url, String key, Storage storage) {
     public record Storage(String bucket) {
     }
