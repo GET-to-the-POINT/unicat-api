819795b3e22d1027cb6c1764b9dcd9eac0cb8791
XIYO <bunny@xiyo.dev>
2025-03-30 20:17:22 +0900

    :fire: 사용하지 않는 임포트

--- src/main/java/gettothepoint/unicatapi/presentation/controller/project/ProjectController.java
+++ src/main/java/gettothepoint/unicatapi/presentation/controller/project/ProjectController.java
@@ -25,7 +25,6 @@ import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
