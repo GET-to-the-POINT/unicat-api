2751a5a71689e7965b7501bdafb770f899c83496
XIYO <bunny@xiyo.dev>
2025-04-28 20:13:44 +0900

    🎨 gradle 설정 및 테스트 태그 제거
    springAiVersion 변수 제거
    JUnitPlatform 태그 제외 설정 제거
    @Tag("manual") 주석 제거

--- src/test/java/gettothepoint/unicatapi/mail/MailServiceManualTest.java
+++ src/test/java/gettothepoint/unicatapi/mail/MailServiceManualTest.java
@@ -24,7 +24,6 @@ import static org.mockito.Mockito.verify;
  * 비동기 메일 전송 수동 통합 테스트
  * 실제 SMTP 서버를 통한 비동기 메일 전송을 검증합니다.
  */
-@Tag("manual")
 @SpringJUnitConfig(classes = {MailSenderAutoConfiguration.class, MailService.class, SyncTaskExecutorConfig.class})
 @TestPropertySource(properties = {
         "spring.mail.host=smtp.gmail.com",
