fec84be5be6beeb54cfe9dc15a747449485ed646
XIYO <bunny@xiyo.dev>
2025-04-29 10:35:27 +0900

    🔧 Implement MailService to handle sending emails asynchronously via event publishing

--- src/main/java/gettothepoint/unicatapi/mail/MailService.java
+++ src/main/java/gettothepoint/unicatapi/mail/MailService.java
@@ -1,7 +1,32 @@
 package gettothepoint.unicatapi.mail;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
 
-public interface MailService {
-    void send(SimpleMailMessage simpleMailMessage);
+@Component
+@RequiredArgsConstructor
+@Profile("mail")
+@Primary
+public class MailService {
+
+    private final ApplicationEventPublisher publisher;
+
+    private final JavaMailSender javaMailSender;
+
+    @Async
+    @EventListener
+    public void handleSendMailEvent(SimpleMailMessage simpleMailMessage) {
+        javaMailSender.send(simpleMailMessage);
+    }
+
+    public void send(SimpleMailMessage simpleMailMessage) {
+        publisher.publishEvent(simpleMailMessage);
+    }
 }
