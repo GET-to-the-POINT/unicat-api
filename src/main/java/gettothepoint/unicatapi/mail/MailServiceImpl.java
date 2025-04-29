b5b0ae4979bc12a1eadc2cd7f5b330d3d5fa9407
XIYO <bunny@xiyo.dev>
2025-04-29 13:42:10 +0900

    Revert "🔧 Implement MailService to handle sending emails asynchronously via event publishing"
    This reverts commit fec84be5be6beeb54cfe9dc15a747449485ed646.

---
+++ src/main/java/gettothepoint/unicatapi/mail/MailServiceImpl.java
@@ -0,0 +1,33 @@
+package gettothepoint.unicatapi.mail;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Profile("mail")
+@Primary
+public class MailServiceImpl implements MailService {
+
+    private final ApplicationEventPublisher publisher;
+
+    private final JavaMailSender javaMailSender;
+
+    @Async
+    @EventListener
+    public void handleSendMailEvent(SimpleMailMessage simpleMailMessage) {
+        javaMailSender.send(simpleMailMessage);
+    }
+
+    @Override
+    public void send(SimpleMailMessage simpleMailMessage) {
+        publisher.publishEvent(simpleMailMessage);
+    }
+}
