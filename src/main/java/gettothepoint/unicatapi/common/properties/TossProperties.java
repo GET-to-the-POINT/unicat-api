e24a616211819b7dc1dea663d4e5d5d1ae62a0da
XIYO <bunny@xiyo.dev>
2025-04-29 15:55:38 +0900

    🔧 Refactor Toss configuration: move Toss properties to a new file and update prefix for clarity

--- src/main/java/gettothepoint/unicatapi/common/properties/TossProperties.java
+++ src/main/java/gettothepoint/unicatapi/common/properties/TossProperties.java
@@ -2,6 +2,6 @@ package gettothepoint.unicatapi.common.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "app.toss")
+@ConfigurationProperties(prefix = "payment.toss")
 public record TossProperties (String clientKey, String secretKey,String approveUrl,String billingUrl){
 }
